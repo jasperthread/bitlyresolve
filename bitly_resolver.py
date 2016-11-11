d72db7a23f5eda71ac95c05a4338d88667fb3891
jasperthread <dagburks@gmail.com>
2016-11-11 16:17:01 -0600

    Add files via upload

---
+++ bitly_resolver.py
@@ -0,0 +1,19 @@
+import requests
+
+thegoods = []
+
+with open('bitlys.txt') as file:
+    for line in file:
+        try:
+            theurl = requests.get(line)
+            resolved = theurl.url
+			# uncomment for the embedded version
+            # resolved = resolved.replace("watch?v=", "embed/")
+            print(line.strip(), "-->", resolved)
+            thegoods.append(resolved)
+        except:
+            print('"' + line.strip() + '" is not a valid url.')
+
+with open('output.txt', 'w') as outfile:
+    for item in thegoods:
+        outfile.write(item + "\n")
